d433aa44093460053ccc86a0710bc13bdc2c9411
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-19 02:33:16 +0300

    Добавил основные стили на проект. Сделал полный сброс

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = gallery.insertAdjacentHTML('afterbegin',
+  createMarkup(galleryItems));
+
+//Создание галереи
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// Создание разметки
+function createMarkup(array) {
+  return array.map(({ original, preview, description } = element) => {
+    return `<li class='gallery__item'>
+  <a class='gallery__link' href='${original}'>
+    <img
+      class='gallery__image'
+      src='${preview}'
+      alt='${description}'
+    />
+  </a>
+</li>`;
+  }).join('');
+}
