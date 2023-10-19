dfae0e17ee4915a257b90b42596cc35d96078641
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-19 07:40:33 +0300

    Закончена секция с видео

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const KEY_CURRENT_TIME = 'videoplayer-current-time';
+
+console.log();
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+if (localStorage.getItem(KEY_CURRENT_TIME)) {
+  const { seconds } = JSON.parse(localStorage.getItem(KEY_CURRENT_TIME))
+  player.setCurrentTime(seconds)
+}
+const onPlay = function(data) {
+  localStorage.setItem(KEY_CURRENT_TIME, JSON.stringify(data));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
 
